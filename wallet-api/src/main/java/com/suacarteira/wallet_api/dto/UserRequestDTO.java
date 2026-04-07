c06ec952e1e7a221594caf6546cbd25a17c97ab6
Jota0x <joaopedrocamp554@gmail.com>
2026-04-07 17:09:57 -0300

    recomeco projeto

--- wallet-api/src/main/java/com/suacarteira/wallet_api/dto/UserRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.suacarteira.wallet_api.dto;
-
-import java.math.BigDecimal;
-
-//Carrega dados de JSON
-//Cria automaticamente os métodos para ler os dados
-public record UserRequestDTO(String titular, String email, BigDecimal saldoInicial){} 
-
-
